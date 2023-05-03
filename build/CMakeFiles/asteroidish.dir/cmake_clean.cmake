5c613cbbdf9deedb245f46462a1f1bab71ddd679
WillyBBlue <skihatduckie@gmail.com>
2023-05-03 14:19:31 -0400

    Add CMake project

---
+++ build/CMakeFiles/asteroidish.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asteroidish.dir/src/main.cpp.obj"
+  "CMakeFiles/asteroidish.dir/src/main.cpp.obj.d"
+  "asteroidish.exe"
+  "asteroidish.exe.manifest"
+  "asteroidish.pdb"
+  "libasteroidish.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/asteroidish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
